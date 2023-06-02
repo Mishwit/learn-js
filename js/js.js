5994f750134d62f1a5a182db1f9a1af58f6e73ee
Mishwit <mgavrilovlife@gmail.com>
2023-06-02 15:34:10 +0200

    JS 1st script (21 lessons)

--- js/js.js
+++ js/js.js
@@ -1,2 +1,39 @@
-const name = "world";
-console.log(`hello ${2 * 2}`);
+const firstName = prompt("Напишите первое имя"),
+  secondName = prompt("Напишите второе имя");
+
+const randomNumber = Math.floor(Math.random() * 100);
+
+if (
+  randomNumber < 33 &&
+  firstName != null &&
+  secondName != null &&
+  firstName != "" &&
+  secondName != ""
+) {
+  alert(
+    `Результат совместимости ${firstName} и ${secondName} равен ${randomNumber} вам нужно поискать человека с другим именем`
+  );
+} else if (
+  randomNumber >= 33 &&
+  randomNumber < 66 &&
+  firstName != null &&
+  secondName != null &&
+  firstName != "" &&
+  secondName != ""
+) {
+  alert(
+    `Результат совместимости ${firstName} и ${secondName} равен ${randomNumber} совместимость хорошая, но может быть и лучше`
+  );
+} else if (
+  randomNumber >= 66 &&
+  firstName != null &&
+  secondName != null &&
+  firstName != "" &&
+  secondName != ""
+) {
+  alert(
+    `Результат совместимости ${firstName} и ${secondName} равен ${randomNumber} вы идеально подходите друг другу`
+  );
+} else {
+  alert("Вы не ввели имя. Попробуйте ещё раз");
+}
