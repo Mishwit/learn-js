c25611268d1b1e8ece6d5e8edaec82fe4d2362be
Mishwit <mgavrilovlife@gmail.com>
2023-08-09 16:51:56 +0200

    rework functions

--- Counter/js.js
+++ Counter/js.js
@@ -1,40 +1,14 @@
 let num = 0;
 
-function inc1() {
-  num += 1;
-  document.getElementById("num").innerHTML = num;
-}
+const inc = (value) => {
+  document.getElementById("num").innerHTML = num += value;
+};
 
-function inc2() {
-  num += 2;
-  document.getElementById("num").innerHTML = num;
-}
+const dec = (value) => {
+  document.getElementById("num").innerHTML = num >= value ? (num -= value) : 0;
+};
 
-function inc3() {
-  num += 3;
-  document.getElementById("num").innerHTML = num;
-}
-
-function dec1() {
-  num > 0 ? (num -= 1) : "";
-  document.getElementById("num").innerHTML = num;
-}
-
-function dec2() {
-  num === 1 ? (num -= 1) : "";
-  num > 0 ? (num -= 2) : "";
-
-  document.getElementById("num").innerHTML = num;
-}
-
-function dec3() {
-  num === 1 ? (num -= 1) : "";
-  num === 2 ? (num -= 2) : "";
-  num > 0 ? (num -= 3) : "";
-  document.getElementById("num").innerHTML = num;
-}
-
-function res() {
+const res = () => {
   num = 0;
   document.getElementById("num").innerHTML = num;
-}
+};
