2c68c7bbe2e10626432354b8f2de6f6ac2a9f9cd
Mishwit <mgavrilovlife@gmail.com>
2023-08-10 13:00:37 +0200

    removed .button_disabled class and script

--- Counter/scripts.js
+++ Counter/scripts.js
@@ -6,13 +6,6 @@ const inc = (value) => {
 
 const dec = (value) => {
   document.getElementById("num").innerHTML = num >= value ? (num -= value) : 0;
-  if (num < value) {
-    let element = document.getElementById("dec2");
-    element.classList.add("button_disabled");
-  } else {
-    let element = document.getElementById("dec2");
-    element.classList.remove("button_disabled");
-  }
 };
 
 const res = () => {
