1f6b440cce5f403a212d5e2b0af186efa5596e12
Mishwit <mgavrilovlife@gmail.com>
2023-09-06 19:00:19 +0200

    120-127 lessons

--- Experiense App/script.js
+++ Experiense App/script.js
@@ -68,3 +68,160 @@ tabContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+//прозрачность навигационного меню
+
+function hover(e, opacity) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest("nav").querySelector(".nav__logo");
+
+    siblings.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+}
+
+nav.addEventListener("mouseover", hover.bind(0.5));
+
+nav.addEventListener("mouseout", hover.bind(1));
+
+//появление меню после прокрутки
+
+const navContainer = document.querySelector(".nav");
+
+function callBack(entries) {
+  if (!entries[0].isIntersecting) {
+    navContainer.classList.add("sticky");
+  } else {
+    navContainer.classList.remove("sticky");
+  }
+}
+
+const options = {
+  threshold: 0,
+  rootMargin: "-90px",
+};
+const observer = new IntersectionObserver(callBack, options);
+observer.observe(document.querySelector(".header"));
+
+//всплытие секций
+
+// const allSections = document.querySelectorAll(".section");
+// function revealSection(entries, observe) {
+//   if (entries[0].isIntersecting) {
+//     entries[0].target.classList.remove("section--hidden");
+//     observe.unobserve(entries[0].target);
+//   }
+// }
+
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   threshold: 0.15,
+// });
+
+// allSections.forEach(function (section) {
+//   sectionObserver.observe(section);
+//   section.classList.add("section--hidden");
+// });
+
+//ленивая подгрузка изображений
+
+const images = document.querySelectorAll("img[data-src]");
+
+function loadImg(entries, observer) {
+  if (!entries[0].isIntersecting) return;
+  entries[0].target.src = entries[0].target.dataset.src;
+
+  entries[0].target.addEventListener("load", function () {
+    entries[0].target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entries[0].target);
+}
+const imgObserver = new IntersectionObserver(loadImg, { threshold: 0.15 });
+
+images.forEach((img) => {
+  imgObserver.observe(img);
+});
+
+//слайдер
+
+const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
+const btnRight = document.querySelector(".slider__btn--right");
+const btnLeft = document.querySelector(".slider__btn--left");
+const dotsContainer = document.querySelector(".dots");
+
+let currSlide = 0;
+const maxSlides = slides.length;
+
+function createDots() {
+  slides.forEach(function (_, i) {
+    dotsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+}
+createDots();
+
+function goToSlide(slide) {
+  slides.forEach(function (s, i) {
+    s.style.transform = `translateX(${100 * (i - slide)}%)`;
+  });
+}
+
+function activateDots(slide) {
+  document.querySelectorAll(".dots__dot").forEach(function (dot) {
+    dot.classList.remove("dots__dot--active");
+  });
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+}
+
+goToSlide(0);
+activateDots(0);
+
+function nextSlide() {
+  if (currSlide === maxSlides - 1) {
+    currSlide = 0;
+  } else {
+    currSlide++;
+  }
+  goToSlide(currSlide);
+  activateDots(currSlide);
+}
+
+function prevSlide() {
+  if (currSlide === 0) {
+    currSlide = maxSlides - 1;
+  } else {
+    currSlide--;
+  }
+  goToSlide(currSlide);
+  activateDots(currSlide);
+}
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    prevSlide();
+  }
+  if (e.key === "ArrowRight") {
+    nextSlide();
+  }
+});
+
+dotsContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const slide = e.target.dataset.slide;
+    goToSlide(slide);
+    activateDots(slide);
+  }
+});
