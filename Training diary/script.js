a336958a7a3ad25162b79af53dce97a72fe63ec1
Mishwit <mgavrilovlife@gmail.com>
2023-10-18 17:13:31 +0200

    153 - complete

--- Training diary/script.js
+++ Training diary/script.js
@@ -25,59 +25,90 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 let map;
 let mapEvent;
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(function (position) {
+
+class App {
+  _map;
+  _mapEvent;
+
+  constructor() {
+    // Запуск логики приложения
+    this._getPosition();
+
+    // Обработчик события который вызывает метод __newWorkout.
+    form.addEventListener("submit", this._newWorkout.bind(this));
+
+    // Обработчик события который вызывает метод _toogleField.
+    inputType.addEventListener("change", this._toggleField);
+  }
+
+  // Метод запроса данных о местоположении от пользовател. В случае успеха, запускается функция _loadMap
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+
+        function () {
+          alert("Вы не предоставили доступ к своей локации");
+        }
+      );
+  }
+
+  // Метод загрузки карты на страницу, в случае положительного ответа о предоставлении своих координат
+  _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
 
-    map = L.map("map").setView(coords, 13);
-    console.log(map);
+    this._map = L.map("map").setView(coords, 13);
+    console.log(this._map);
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this._map);
 
-    map.on(
-      "click",
-      function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove("hidden");
-        inputDistance.focus();
-      },
+    //Обработчик события нажатия по карте, который запустит метод _showForm
+    this._map.on("click", this._showForm.bind(this));
+  }
 
-      function () {
-        alert("Вы не предоставили доступ к своей локации");
-      }
-    );
+  //Метод который отобразит форму при клике по карте.
+  _showForm(mapE) {
+    this._mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
 
-    form.addEventListener("submit", function (e) {
-      e.preventDefault();
-      inputDistance.value =
-        inputDuration.value =
-        inputCadence.value =
-        inputElevation.value =
-          "";
-      console.log(mapEvent);
-      const { lat, lng } = mapEvent.latlng;
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: " mark-popup",
-          })
-        )
-        .setPopupContent("Тренировка")
-        .openPopup();
-    });
-  });
+  // Метод который переключает типы тренировок.
+  _toggleField() {
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  }
 
-inputType.addEventListener("change", function () {
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-});
+  // Метод который установит маркер на карту.
+  _newWorkout(e) {
+    e.preventDefault();
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
+    console.log(this._mapEvent);
+    const { lat, lng } = this._mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this._map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: " mark-popup",
+        })
+      )
+      .setPopupContent("Тренировка")
+      .openPopup();
+  }
+}
+
+// Запуск приложения
+const app = new App();
